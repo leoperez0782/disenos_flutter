e34b142f6e1446f074df171f0fd0c71a86a6e1c4
Leonardo Perez <leoperez0782@hotmail.com>
2021-02-26 21:32:27 -0300

    agregados botones con blur

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:disenos_flutter/src/pages/basic_page.dart';
+import 'package:disenos_flutter/src/pages/botones_page.dart';
 import 'package:disenos_flutter/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Esto cambia el color de la barra.
+    // SystemChrome.setSystemUIOverlayStyle(
+    //     SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.black));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basic': (BuildContext context) => BasicPage(),
         'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage()
       },
     );
   }
